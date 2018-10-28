a7c426d1969ddda9af75d56229ad1f237a99f596
Bowen Tan <tanbowen@umich.edu>
2018-10-27 21:03:24 -0400

    get notes restAPI

--- WLTTBackend/WLTTBackend/api/hello.py
+++ WLTTBackend/WLTTBackend/api/hello.py
@@ -12,11 +12,12 @@ def hello_world_func():
     print("entered hello\n")
     return flask.jsonify(dict), 206
 
-@WLTTBackend.app.route('/api/hello', methods=["GET"])
-def hello_world_func():
-    dict = {
-        "text": "hello world",
-        "id": 1
-    }
-    print("entered hello\n")
-    return flask.jsonify(dict), 206
+@WLTTBackend.app.route('/api/getnote/all/', methods=["GET"])
+def get_posts():
+    context = {}
+    connection = WLTTBackend.model.get_db()
+    cur = connection.execute('''SELECT * FROM NOTES N;''')
+    notes = cur.fetchall()
+    context["notes"] = notes
+    return flask.jsonify(**context)
+
