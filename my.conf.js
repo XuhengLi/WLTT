d369c6cd8ddc5cb131d8202319fda55a65e636b8
Xuheng Li <xl2784@columbia.edu>
2018-10-30 21:01:28 -0400

    use broswerify instead of webpack

--- my.conf.js
+++ my.conf.js
@@ -10,13 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
     files: [
       'js/*.js',
-      './app.js',
       'spec/*.js'
     ],
 
@@ -29,7 +28,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        './spec/*.js': ['webpack']
+        './spec/*.js': ['browserify'],
+        './js/*.js': ['browserify']
     },
 
     // test results reporter to use
@@ -57,7 +57,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chrome'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
